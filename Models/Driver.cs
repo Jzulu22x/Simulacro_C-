78e77457d1b49b43053eeb01f7cd8dbad588b6e4
Jzulu22x <juanzr1015@gmail.com>
2024-08-02 07:34:43 -0500

    feat: Creation of the class Driver

---
+++ Models/Driver.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_C_.Models;
+public class Driver : User
+{
+    public string?  LicenseNumber { get; set; }
+    public string?  LicenseCategory { get; set; }
+    public int DrivingExperience { get; set; }
+
+    public Driver(string? name, string? lastName, string? typeDocument, string? identificationNumber, DateOnly birthDate, string? email, string? phoneNumber, string? address, string? licenseNumber, string? licenseCategory, int drivingExperience) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, address)
+    {
+        LicenseNumber = licenseNumber;
+        LicenseCategory = licenseCategory;
+        DrivingExperience = drivingExperience;
+    }
+
+    public void UpdateLicenseCategory(string newCategory)
+    {
+        Console.WriteLine($"License category updated from: {LicenseCategory} to: {newCategory}");
+        LicenseCategory = newCategory;
+    }
+
+    public void AddExperience(int years)
+    {
+        Console.WriteLine($"Driving experience increased from: {years} to: {DrivingExperience += years}");
+        DrivingExperience += years;
+    }    
+}
