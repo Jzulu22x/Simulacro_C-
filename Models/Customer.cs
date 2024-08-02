e85dea5f30e4ec82e8cba79bcc1b7fbb8f4fd36e
Jzulu22x <juanzr1015@gmail.com>
2024-08-02 07:43:52 -0500

    feat: Creation of the class Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacro_C_.Models;
+public class Customer : User
+{
+    public string? MembershipLevel { get; set; }
+    public string? PreferredPaymentMethod { get; set; }
+
+    public Customer(string? name, string? lastName, string? typeDocument, string? identificationNumber, DateOnly birthDate, string? email, string? phoneNumber, string? address, string? membershipLevel, string? preferredPaymentMethod) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, address)
+    {
+        MembershipLevel = membershipLevel.ToLower();
+        PreferredPaymentMethod = preferredPaymentMethod.ToLower();
+    }
+
+    public void UpdateMembershipLevel()
+    {
+        if(MembershipLevel == "premium")
+        {
+            Console.WriteLine("The membership level cannot be upgraded because it is already a premium membership"); 
+        }
+        else 
+        {
+            Console.WriteLine($"Membership level updated from: {MembershipLevel} to: premium");
+            MembershipLevel = "premium";
+        }
+    }
+}
